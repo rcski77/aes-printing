e33444983e237833f8680ad876627ea62d8d97c7
rcski77 <rcski77@gmail.com>
2024-06-24 11:24:30 -0400

    added while loop with timer

--- court_api_import.py
+++ court_api_import.py
@@ -1,7 +1,7 @@
 import requests
-from datetime import datetime, timedelta
+import time as t
+from datetime import datetime
 from termcolor import colored
-
 
 class Match:
     def __init__(self, timestamp, result=False, team1="null", team2="null"):
@@ -22,43 +22,49 @@ class Court:
         
 url = "https://results.advancedeventsystems.com/api/event/PTAwMDAwMzY0NDE90/courts/2024-06-24/300"
 time_offset = 3600000
-new_grid = []
 needed = [
-    Court("OCCC North 74", Match(datetime.strptime('2024-06-24 10:30:00', '%Y-%m-%d %H:%M:%S'))),
-    Court("OCCC North 50", Match(datetime.strptime('2024-06-24 10:30:00', '%Y-%m-%d %H:%M:%S'))),
-    Court("OCCC North 17", Match(datetime.strptime('2024-06-24 11:00:00', '%Y-%m-%d %H:%M:%S'))),
-    Court("OCCC North 17", Match(datetime.strptime('2024-06-24 12:00:00', '%Y-%m-%d %H:%M:%S'))),
     Court("OCCC North 12", Match(datetime.strptime('2024-06-24 11:00:00', '%Y-%m-%d %H:%M:%S'))),
     Court("OCCC North 23", Match(datetime.strptime('2024-06-24 11:00:00', '%Y-%m-%d %H:%M:%S'))),
     Court("OCCC North 2", Match(datetime.strptime('2024-06-24 11:00:00', '%Y-%m-%d %H:%M:%S'))),
+    Court("OCCC North 4", Match(datetime.strptime('2024-06-24 11:00:00', '%Y-%m-%d %H:%M:%S'))),
+    Court("OCCC North 54", Match(datetime.strptime('2024-06-24 11:30:00', '%Y-%m-%d %H:%M:%S'))),
+    Court("OCCC North 31", Match(datetime.strptime('2024-06-24 11:30:00', '%Y-%m-%d %H:%M:%S'))),
+    Court("OCCC North 78", Match(datetime.strptime('2024-06-24 12:00:00', '%Y-%m-%d %H:%M:%S'))),
+    Court("OCCC North 5", Match(datetime.strptime('2024-06-24 11:00:00', '%Y-%m-%d %H:%M:%S'))),
+    Court("OCCC North 3", Match(datetime.strptime('2024-06-24 12:00:00', '%Y-%m-%d %H:%M:%S'))),
     ]
 
-courtgrid = requests.get(url).json()
+while True:
+    new_grid = []
+    courtgrid = requests.get(url).json()
 
-for court in courtgrid.get('CourtSchedules'):
-    court_object = Court(court.get('Name'), [])
-    new_grid.append(court_object)
+    for court in courtgrid.get('CourtSchedules'):
+        court_object = Court(court.get('Name'), [])
+        new_grid.append(court_object)
 
-    for match in court.get('CourtMatches'):
-        team1 = match.get('FirstTeamText')
-        team2 = match.get('SecondTeamText')
-        time = datetime.fromtimestamp((match.get('ScheduledStartDateTime') - time_offset) / 1e3)
-        match_object = Match(time, match.get('HasOutcome'), team1, team2)
-        court_object.matches.append(match_object)
-        
-print()
-for scoresheet in needed:
-    print("Looking for match:")
-    print(scoresheet)
-    print(scoresheet.matches)
-
-    for each in new_grid:
-        if each.name == scoresheet.name:
-            for match in each.matches:
-                if scoresheet.matches.timestamp == match.timestamp:
-                    if "Winner" in match.team1 or "Winner" in match.team2 or "Loser" in match.team1 or "Loser" in match.team2:
-                        print(colored("Match NOT ready", 'red'))
-                    else:
-                        print(colored("Scoresheet ready, PRINT!", 'green'))
-                        break         
+        for match in court.get('CourtMatches'):
+            team1 = match.get('FirstTeamText')
+            team2 = match.get('SecondTeamText')
+            time = datetime.fromtimestamp((match.get('ScheduledStartDateTime') - time_offset) / 1e3)
+            match_object = Match(time, match.get('HasOutcome'), team1, team2)
+            court_object.matches.append(match_object)
+            
     print()
+    for scoresheet in needed:
+        print("Looking for match:")
+        print(scoresheet)
+        print(scoresheet.matches)
+
+        for each in new_grid:
+            if each.name == scoresheet.name:
+                for match in each.matches:
+                    if scoresheet.matches.timestamp == match.timestamp:
+                        if "Winner" in match.team1 or "Winner" in match.team2 or "Loser" in match.team1 or "Loser" in match.team2:
+                            print(colored("Match NOT ready", 'red'))
+                        else:
+                            print(colored("Scoresheet ready, PRINT!", 'green'))
+                            break         
+        print()
+        
+    print(colored("Waiting 60 seconds...", 'yellow'))
+    t.sleep(60)
